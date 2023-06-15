f00bdde2fa3f58b5ebbe370f4c7b31ec63b72ef5
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2023-06-14 21:42:19 -0400

    Update 01581-customer-who-visited-but-did-not-make-any-transactions.sql

--- 01581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 01581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -2,3 +2,8 @@
 # amazon- 3
 # nerdwallet- 1
 
+select customer_id, count(visit_id) as count_no_trans 
+from Visits v left join Transactions t
+using(visit_id)
+where transaction_id is null
+group by customer_id
