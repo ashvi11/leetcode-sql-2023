abf1188df4d7597033057dc7aa47fedeb7a0bf86
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2023-06-14 20:41:59 -0400

    Update 00511-game-play-analysis-i.sql

--- 00511-game-play-analysis-i.sql
+++ 00511-game-play-analysis-i.sql
@@ -1,8 +1,8 @@
-#easy
-#bloomberg- 2
-#adobe- 2
-#amazon- 5
-#gsn_games- 1
+# easy
+# bloomberg- 2
+# adobe- 2
+# amazon- 5
+# gsn_games- 1
 
 select player_id, min(event_date) as first_login 
 from Activity
